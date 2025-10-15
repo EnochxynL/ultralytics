e3a94053046b394af7d032f2586c4ce267eeee66
Xie Yinuo <136900616+EnochxynL@users.noreply.github.com>
2025-10-15 12:22:04 +0800

    数据集第一版

--- custom.py
+++ custom.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
 
     # Train the model
     train_results = model.train(
-        data="cirno.yaml",  # path to dataset YAML
+        data="custom/cirno-basketball/data.yaml",  # path to dataset YAML
         epochs=64,  # number of training epochs
         imgsz=640,  # training image size
         device=0  # device to run on, i.e. device=0 or device=0,1,2,3 or device=cpu
---
+++ custom/cirno-basketball/README.dataset.txt
@@ -0,0 +1,6 @@
+# Tracer - basketball > 2023-03-01 10:18pm
+https://universe.roboflow.com/uc-berkely-w210-tracer/tracer-basketball
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
